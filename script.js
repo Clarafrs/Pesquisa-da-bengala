89984b789d8608dc860bc38e69362aa2fe079deb
Clarafrs <clara.ferreira.soares@escola.pr.gov.br>
2026-08-03 10:49:05 -0300

    .

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ function alternarContraste() {
 let tamanhoFonteAtual = 100; // Porcentagem inicial
 
 function aumentarTexto() {
-    if (tamanhoFonteAtual < 150) { // Limite máximo de 150%
+    if (tamanhoFonteAtual < 150) { // Limite maximo de 150%
         tamanhoFonteAtual += 10;
         document.body.style.fontSize = tamanhoFonteAtual + '%';
     }
 }
 
 function diminuirTexto() {
-    if (tamanhoFonteAtual > 80) { // Limite mínimo de 80%
+    if (tamanhoFonteAtual > 80) { // Limite minimo de 80%
         tamanhoFonteAtual -= 10;
         document.body.style.fontSize = tamanhoFonteAtual + '%';
         <script src="js/script.js"></script>
